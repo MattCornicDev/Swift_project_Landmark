d685be8f956e049533059ed625ccf5ba4c0ed7b1
Rarib LAAMIMAT <glgharib@gmail.com>
2021-06-12 14:43:00 +0200

    Initial Commit

---
+++ AppSwiftLandmarks/AppSwiftLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftLandmarksApp.swift
+//  AppSwiftLandmarks
+//
+//  Created by Matt Cornic on 12/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
